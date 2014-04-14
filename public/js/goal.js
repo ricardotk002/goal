a019845436f2432e2a891e24af61f77eefe6f68a
Ricardo Díaz <ricardotk002@gmail.com>
2014-04-13 21:26:10 -0500

    first commit

---
+++ public/js/goal.js
@@ -0,0 +1,39 @@
+var app = angular.module('goal', []);
+
+app.controller('MainController', function($scope, $http) {
+	$scope.date = new Date();
+	$scope.formData = {};
+
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('error' + data);
+		});
+
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('error' + data);
+			});
+	};
+
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos' + id)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('error' + data);
+			});
+	};
+});
---
+++ routes/todo.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://usrgoal:goalx@novus.modulusmongo.net:27017/zovy8Sev')
+
+var Todo = mongoose.model('Todo', {
+    text : String
+});
+
+exports.get = function(req, res){
+	Todo.find(function(err, todos) {
+		if(err)
+			req.send(err);
+		res.json(todos)
+	});
+};
+
+exports.post = function(req, res) {
+	Todo.create({
+		text: req.body.text,
+		done: false
+	}, function(err, todo) {
+		if(err)
+			req.send(err);
+
+		Todo.find(function(err, todos) {
+			if(err)
+				req.send(err);
+			res.json(todos);
+		});
+	})
+};
+
+exports.delete = function(req, res) {
+	Todo.remove({
+		_id: req.params.todo_id
+	}, function(err, todo) {
+		if(err)
+			req.send(err)
+
+		Todo.find(function(err, todos) {
+			if(err)
+				req.send(err);
+			req.json(todos);
+		});
+	});
+}
